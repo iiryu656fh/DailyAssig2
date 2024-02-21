1aa91ceae62adeb66871b6a39847826b299e1c5a
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-02-21 16:40:14 +0700

    modif

---
+++ Src/Jobsheet2/Buku13.java
@@ -0,0 +1,32 @@
+public class Buku14 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            if (jml <= stok) {
+                stok -= jml;
+            } else {
+                System.out.println("Stok kurang");
+            }
+        } else {
+            System.out.println("Stok buku habis");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
