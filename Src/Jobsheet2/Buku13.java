ef404a67b62c4f877633540ce599d5c57dd4c805
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-02-22 07:32:33 +0700

    konstruktor

--- Src/Jobsheet2/Buku13.java
+++ Src/Jobsheet2/Buku13.java
@@ -2,6 +2,18 @@ public class Buku13 {
     String judul, pengarang;
     int halaman, stok, harga;
 
+    public Buku13() {
+
+    }
+
+    public Buku13(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
