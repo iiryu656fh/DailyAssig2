aefeb099470f768345a0845588d767a790fa4dbc
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-02-28 21:54:33 +0700

    modif

--- Src/Jobsheet3/ArrayObjects.java
+++ Src/Jobsheet3/ArrayObjects.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class ArrayObjects{
     public static void main(String[] args) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];        
-
         Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < 3; i++) {
+        System.out.print("Masukkan panjang array: ");
+        int length = sc.nextInt();
+        PersegiPanjang[] ppArray = new PersegiPanjang[length];        
+
+        for(int i = 0; i < length; i++) {
             ppArray[i] = new PersegiPanjang();
             System.out.println("Persegi Panjang ke-" + i);
             System.out.print("Masukkan panjang: ");
@@ -14,7 +16,7 @@ public class ArrayObjects{
             ppArray[i].lebar = sc.nextInt();
         }
 
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < length; i++) {
             System.out.println("Persegi Panjang ke-" + i);
             System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
         }
