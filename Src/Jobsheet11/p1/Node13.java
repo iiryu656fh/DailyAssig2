4418cfce315061ebe44276b3dc880d1aa91492d9
iiryu656fh <khoirotunnisa2507@gmail.com>
2024-05-29 21:55:50 +0700

    jobsheet11

---
+++ Src/Jobsheet11/p1/Node13.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class Node13 { 
+    int data;
+    Node13 left;
+    Node13 right;
+
+    public Node13 (int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
